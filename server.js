63a094bd7a2ed330d313cead8ddb2fd7976b203d
vikas <vikas.a.sharma@rbs.co.uk>
2020-07-23 08:50:31 +0530

    final commit

--- server.js
+++ server.js
@@ -3,6 +3,6 @@
 const server = require('./app');
 const port = 8089;
 
-server.listen(port, 'devecpvm006877', function() {
+server.listen(port, 'localhost', function() {
   console.log('Server running on port: %d', port);
 });
